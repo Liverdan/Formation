3c90b67be9cb7023d893410cb985c927b770fb88
Pierre Vanlierde <liverdan@ntymail.com>
2018-06-21 17:04:10 +0200

    mise en place $.ajax avec fichier json + formulaire.

--- form.php
+++ form.php
@@ -26,7 +26,7 @@
 		$txtBtn = "Corriger";
 		$sql = "INSERT INTO users (name, firstname, mail, niveau) VALUES ('$name', '$firstname','$mail','$niveau') ON DUPLICATE KEY UPDATE name='$name',firstname='$firstname',niveau='$niveau'";
 		if ($connexion -> query($sql)) {
-			echo '<div id="dialog" title="Confirmation"<p>Merci pour votre participation</p></div>';
+			echo '<div id="dialog" title="Confirmation"><p>Merci pour votre participation</p></div>';
 		} else {
 			echo "<p>Requete planté</p>";
 		}
@@ -34,43 +34,76 @@
 	$connexion -> close();
     ?>
 
-    <p><form method="post">
-    <label>Votre nom : <input type="text" name="name" placeholder="Nom" value="<?php echo $name ?>"/></label>
-    <?php
-	if (!$name && count($_POST)) {
-		echo sprintf('<span>%s nom</span>', $alerte);
-	}
-    ?><br />
-
-    <label>Votre pénom : <input type="text" name="firstname" placeholder="Prénom" value="<?php echo $firstname ?>"/></label>
-    <?php
-	if (!$firstname && count($_POST)) {
-		echo sprintf('<span>%s prénom</span>', $alerte);
-	}
-    ?><br />
-    <label>Votre e-mail : <input type="email" name="mail" placeholder="e-mail" value="<?php echo $mail ?>"/></label>
-    <?php
-	if (!$mail && count($_POST)) {
-		echo sprintf('<span>%s mail</span>', $alerte);
-	}
-    ?><br />
-
-    <label>Votre niveau : <input type="range" name="niveau" min="0" max="10" step="1" value="<?php echo $niveau ?>"/></label><br />
-
-    <!--p><input type="submit" value="<?php echo $txtBtn ?>"/></p> -- > ancien input-->
-
-    <div class="widget">
-    <button id="opener" class="ui-button ui-widget ui-corner-all" type="submit"><?php echo $txtBtn ?></button>
-    </div>
-    </form></p>
-    </div>
-    <div class="json-content">
-    <script>
-        
-    </script>
     <p>
-        Json
+    	<form method="post">
+		    <label>Votre nom : <input type="text" name="name" placeholder="Nom" value="<?php echo $name ?>"/></label>
+		    <?php
+			if (!$name && count($_POST)) {
+				echo sprintf('<span>%s nom</span>', $alerte);
+			}
+		    ?><br />
+		
+		    <label>Votre pénom : <input type="text" name="firstname" placeholder="Prénom" value="<?php echo $firstname ?>"/></label>
+		    <?php
+			if (!$firstname && count($_POST)) {
+				echo sprintf('<span>%s prénom</span>', $alerte);
+			}
+		    ?><br />
+		    <label>Votre e-mail : <input type="email" name="mail" placeholder="e-mail" value="<?php echo $mail ?>"/></label>
+		    <?php
+			if (!$mail && count($_POST)) {
+				echo sprintf('<span>%s mail</span>', $alerte);
+			}
+		    ?><br />
+		
+		    <label>Votre niveau : <input type="range" name="niveau" min="0" max="10" step="1" value="<?php echo $niveau ?>"/></label><br />
+		    <!--p><input type="submit" value="<?php echo $txtBtn ?>"/></p> -- > ancien input-->
+		    <div class="widget">
+		    	<button id="opener" class="ui-button ui-widget ui-corner-all" type="submit"><?php echo $txtBtn ?></button>
+			</div>
+		</form>
+	</p>
+</div>
+
+<div class="json-content">
+	<p class="contentJS">Json
+    	<form id="userForm">
+    		<label>Prénom</label><input type="text" name="$firstName" placeholder="Prénom"/>
+    		<input type="text" name="lastName" placeholder="Nom"/>
+    		<input type="text" name="age" placeholder="Age"/>
+    		<input type="text" name="sex" placeholder="Sexe"/>
+    		<button class="ui-button ui-widget ui-corner-all" type="submit">Envoyer</button>
+    		<span id="error"></span>
+    	</form>        
     </p>
+    <script>
+    	var form=$('#userForm');
+    	var errorSpan = $('#error');
+    	var url='http://formation/bap_users_formation.json';
+    	
+    	var successCallBack=function(response){
+					console.log(response);
+		}
+		var errorCallBack=function(response){
+					errorSpan.append(response.responseText);
+    	}
+    	var getOption = { //argument de la req $.ajax
+	    		method:'POST',
+	    		async:true,
+	    		cache:false,
+	    		success:successCallBack,
+	    		error:errorCallBack,
+    	};
+    	//$.get(url, successCallBack); <=== méthode get simple
+    	form.on('submit',function (e){ // fonction pour récuperer les datas du formulaire
+    		e.preventDefault();// pour ne pas recharger la page
+    		getOption.data = $(this).serialize(); //serialise les arg de la var getOption
+    		$.ajax(url, getOption);//envoi les données en fonction des arg
+    	});
+    	
+    	//console.log($);
+    </script>
+    
 
 </div>
 <hr />
