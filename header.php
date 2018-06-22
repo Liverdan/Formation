15191c8ce20b03068797c8f1d9189ebed420b9cc
Pierre Vanlierde <liverdan@ntymail.com>
2018-06-22 13:37:36 +0200

    Ajout et suppr de la même ligne

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 		<script
 			  src="https://code.jquery.com/jquery-3.3.1.js"
 			  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-			  crossorigin="anonymous"></script>	
+			  crossorigin="anonymous"></script>		
 		<script src="js/jquery-ui.js"></script>
 		<script src="js/executeJQ.js"></script>
 	</head>
