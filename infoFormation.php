d822878b3011e8544c48b396d5cd836ed54fd3a8
Pierre Vanlierde <liverdan@ntymail.com>
2018-07-04 17:23:02 +0200

    Ajustement formulaire

--- infoFormation.php
+++ infoFormation.php
@@ -10,6 +10,7 @@ include 'acces_db.php';
 	$name = '';
 	$mail = '';
 	$niveau = 5;
+	$choix=0;
 	$txtBtn = 'Envoyer';
 	$alerte = "Vous devez compléter votre ";
 	// test de l'existance de la variable dans l'array $data, si (?) vrai sinon (:)
@@ -18,7 +19,6 @@ include 'acces_db.php';
 	$mail = isset($data['mail']) ? $data['mail'] : '';
 	$niveau = isset($data['niveau']) ? $data['niveau'] : 5;
 	$connexion = new mysqli(HOST, USER, PWD, DB);
-	//echo sprintf('<h3>Monsieur%s %s votre e-mail %s</h3>', $name, $firstname, $mail);
 	if ($connexion -> error) {
 		echo "Connection échoué";
 		die('Error connecton base : ' . $connexion -> error);
@@ -29,20 +29,23 @@ include 'acces_db.php';
 		$sqlaf = $connexion->query("SELECT * FROM categories ORDER BY Pos DESC");
 			$sqlaf->data_seek(0);
 			while ($row = $sqlaf->fetch_assoc()) {
-				$nChoix= "choix".$row['Pos'];
-				$nLevel="niveau".$row['Pos'];
-		    	echo "<li>".$nChoix." ".$row['id'] ." ". $row['cat']." ".$nLevel." ".$row['level']. "<li>";
+				$nChoix= $row['id'];
+				$nLevel=$row['Pos'];
+		    	echo "<li>".$nChoix." ".$row['id'] ." ". $row['cat']." ".$nLevel." ".$row['level']. "</li>";
 			};
+		$choix=$nChoix;
 		$txtBtn = "Corriger";
-		$sqlins = "INSERT INTO users (name, firstname, mail) VALUES ('$name', '$firstname','$mail') ON DUPLICATE KEY UPDATE name='$name',firstname='$firstname'";
+		$sqlins = "INSERT INTO users (name, firstname, mail,choix_1) VALUES ('$name', '$firstname','$mail','$choix') ON DUPLICATE KEY UPDATE name='$name',firstname='$firstname',choix_1=$choix";
 		if ($connexion -> query($sqlins)) {
+			echo "<p>Requete Ins ok</p>".$choix;
 			//echo "<div id='dialog' title='Confirmation'><p>Merci pour votre participation</p></div>";
 		} else {
-			echo "<p>Requete planté</p>";
+			echo "<p>Requete Ins planté</p>";
 		}
 	}
 	//$connexion -> close();
     ?>
+    
     <?php 
 //Update de la liste catégorie dans la table categories
 //$connexion = new mysqli(HOST, USER, PWD, DB);
@@ -55,9 +58,9 @@ include 'acces_db.php';
 		extract($c);
 		$sqlcat=("UPDATE categories SET cat='$cat', Pos=$Pos, level=$level WHERE id=$id");
 		if ($connexion->query($sqlcat)=== TRUE){
-			$res ="Requete Ok";
+			$res ="Requete Cat Ok";
 		}else{
-			$res= "Requete Ko";
+			$res= "Requete Cat Ko";
 		}
 	}
 	//$connexion->close();
@@ -102,7 +105,8 @@ include 'acces_db.php';
 				<li>
 					<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
 			    	<input type="hidden" name="Categorie[<?php echo $i;?>][id]" value="<?php echo $row["id"];?>"/>
-			    	<input type="text" name="Categorie[<?php echo $i;?>][cat]" value="<?php echo $row["cat"];?>"/> 
+			    	<input type="text" name="Categorie[<?php echo $i;?>][cat]" value="<?php echo $row["cat"];?>"/>
+			    	<input type="hidden" name="Categorie[<?php echo $i;?>][mail]" value="<?php echo isset($data['mail']) ? $data['mail'] : '';?>"/>
 			    	<input type="hidden" class="positionInput" name="Categorie[<?php echo $i;?>][Pos]" value="<?php echo $row["Pos"];?>"/>
 			    	<span>0</span>
 			    	<input type="range" id="custom-handle" class="ui-slider-handle" name="Categorie[<?php echo $i;?>][level]" min="0" max="10" step="1" value="<?php echo $row["level"];?>"/>
@@ -129,70 +133,27 @@ include 'acces_db.php';
 			$sqlw = $connexion->query("SELECT * FROM categories ORDER BY Pos DESC");
 			$sqlw->data_seek(0);
 			while ($row = $sqlw->fetch_assoc()) {
-				$nChoix= "choix".$row['Pos'];
-				$nLevel="niveau".$row['Pos'];
 		    	$cate=$_POST["Categorie"];
 	foreach ($cate as $c){
 		extract($c);
-		$sqlu=("UPDATE users SET Choix-1=$id WHERE name='$name'");
+		$i=$i+1;
+		$choixn="choix_".$i;
+		$priorityn="Priority_".$i;
+		$leveln="niveau_".$i;		
+		$sqlu=("UPDATE users SET $choixn=$id, $priorityn=$Pos, $leveln=$level WHERE mail='$mail'");
 		if ($connexion->query($sqlu)=== TRUE){
-			$res ="ok";
+			$res ="Requete U ok";
 		}else{
-			$res= "Ko";
+			$res= "Requete U Ko";
 		}
 		}
+		$i=0;
 		};
 	
 	$connexion->close();
 	print_r($res);
 }
  ?>
- <!--*************************************************-->
-<!--div class="pied_page"	
-	<div id="infoFormation">
-		<ul>
-			<?php
-			$connexion = new mysqli(HOST, USER, PWD, DB);
-			$sqlaf = $connexion->query("SELECT * FROM categories ORDER BY Pos DESC");
-			$sqlaf->data_seek(0);
-			while ($row = $sqlaf->fetch_assoc()) {
-				$nChoix= "choix".$row['Pos'];
-				$nLevel="niveau".$row['Pos'];
-		    	echo "<li>".$nChoix." ".$row['id'] ." ". $row['cat']." ".$nLevel." ".$row['level']. "<li>";
-			};
-			?>
-		</ul>
-		&nbsp;
-		
-		<!--form method="post" action="infoFormation.php">
-			<h2>Vos priorités et niveaux</h2>
-			<p>Classer par ordre croissant, en déplacant les flèches suivantes <span class="ui-icon ui-icon-arrowthick-2-n-s"></span> votre choix de formation.</p>
-			<p><i>(Le premier étant le plus important)</i></p>
-			<ul id="sortable">
-				<?php
-				$i=0;
-				$connexion = new mysqli(HOST, USER, PWD, DB);
-				$sql = $connexion->query("SELECT * FROM categories ORDER BY Pos DESC");
-				$sql->data_seek(0);
-				while ($row = $sql->fetch_assoc()) {
-					$i++;
-				?>
-				<li>
-					<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-			    	<input type="hidden" name="Categorie[<?php echo $i;?>][id]" value="<?php echo $row["id"];?>"/>
-			    	<input type="text" name="Categorie[<?php echo $i;?>][cat]" value="<?php echo $row["cat"];?>"/> 
-			    	<input type="hidden" class="positionInput" name="Categorie[<?php echo $i;?>][Pos]" value="<?php echo $row["Pos"];?>"/>
-			    	<span>0</span>
-			    	<input type="range" id="custom-handle" class="ui-slider-handle" name="Categorie[<?php echo $i;?>][level]" min="0" max="10" step="1" value="<?php echo $row["level"];?>"/>
-			    	<span>10</span>
-				</li>
-				<?php
-				}
-				?>
-			</ul> 
-			<button class="ui-button ui-widget ui-corner-all" type="submit">Enregistrer</button>
-		</form>
-	<div/-->
 <?php
 echo "<div/>";
 include 'footer.php';
